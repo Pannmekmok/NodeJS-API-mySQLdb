311d8e4693ade058f668b176abb9efff6e0e7634
Pannmekmok <88367141+Pannmekmok@users.noreply.github.com>
2023-06-01 16:56:25 +0700

    Add files via upload

---
+++ testdb.js
@@ -0,0 +1,28 @@
+import mysql2 from 'mysql2'
+import express from 'express'
+
+const connection = mysql2.createConnection({
+    host: "localhost",
+    database: "test_db",
+    user: "root",
+    password: "Nongpann1!"
+});
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connection.connect((err)=> {
+        if (err) throw err;
+        console.log("DATABASE CONNECTED");
+    });
+});
+
+app.use("/all", (req, res)=> {
+    const sql_query = 'select * from test_db.test'
+    connection.query(sql_query, (err, result)=> {
+        if (err) throw err;
+        res.send(result)
+    });
+});
